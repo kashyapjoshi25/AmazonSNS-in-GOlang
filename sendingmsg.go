4e139152b40306625c1b5a51dded7488157c619e
Kashyap Joshi <115873374+kashyapjoshi25@users.noreply.github.com>
2023-06-02 17:26:09 +0530

    Add files via upload

--- sendingmsg.go
+++ sendingmsg.go
@@ -12,8 +12,8 @@ import (
 func main() {
 	// Create a new AWS session
 	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String("ap-south-1"),                                                                                 // Replace with your desired AWS region
-		Credentials: credentials.NewStaticCredentials("AKIAVGIMFRGDZUUMGGHW", "43GGJBJFsfdh342hPgDXf1IcG5V1+s3NrW+v2zDw", ""), // Uncomment if not using shared credentials file
+		Region:      aws.String("ap-south-1"),                                                    // Replace with your desired AWS region
+		Credentials: credentials.NewStaticCredentials("AWS ACCESS KEY", "AWS ACCESS KEY ID", ""), // Uncomment if not using shared credentials file
 	})
 	if err != nil {
 		fmt.Println("Failed to create session:", err)
@@ -24,6 +24,7 @@ func main() {
 	svc := sns.New(sess)
 
 	// Specify the topic ARN you want to send the message to
+	//Able to find in aws console under aws sns topic
 	topicARN := "arn:aws:sns:ap-south-1:357044685191:Testing_SNS"
 
 	// Specify the message you want to send
